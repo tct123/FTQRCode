1a0d19f5783939cff4ab134bfc93c3c54f50e56b
tct123 <42028373+tct123@users.noreply.github.com>
2024-06-01 08:54:17 +0200

    .

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 import flet as ft
 import qrcode
+import qrcode.image.svg
 from datetime import datetime
+from mylocale.TR import tr
 
 
 def main(page: ft.Page):
     def generate(e):
         filename = datetime.now()
+        if method == "basic":
+            # Simple factory, just a set of rects.
+            factory = qrcode.image.svg.SvgImage
+        elif method == "fragment":
+            # Fragment factory (also just a set of rects)
+            factory = qrcode.image.svg.SvgFragmentImage
+        else:
+            # Combined path factory, fixes white space that may occur when zooming
+            factory = qrcode.image.svg.SvgPathImage
+
         img = qrcode.make(data=url.value)
         name = f"{filename}qr.png"
         img.save(name)
@@ -15,7 +27,7 @@ def main(page: ft.Page):
     page.title = "FTQRCode"
     page.appbar = ft.AppBar(title=ft.Text(page.title))
     qrcode_img = ft.Image("assets/icon.png", width=100, height=100)
-    url = ft.TextField()
+    url = ft.TextField(label="Value")
     btn = ft.TextButton(text="Generate", on_click=generate)
     page.add(ft.SafeArea(ft.Column(controls=[qrcode_img, url, btn])))
 
